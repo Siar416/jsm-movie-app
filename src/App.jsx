34244f0864b095d6439ef5c23ac5eaf006e54b93
Siar Wahidi <siar.wahidi@gmail.com>
2022-06-07 16:14:04 -0400

    useEffect to fetch movies from api based on title

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import React, { useEffect } from "react";
 const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`;
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
   return <h1>App</h1>;
 };
 
