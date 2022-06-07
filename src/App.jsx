dbd3d840219c065f05bb1fbf7d35c1d481e7116d
Siar Wahidi <siar.wahidi@gmail.com>
2022-06-07 16:08:31 -0400

    intial commit. Clean up, along with creating env to hold api key

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+
+const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`;
+
+const App = () => {
+  return <h1>App</h1>;
+};
+
+export default App;
